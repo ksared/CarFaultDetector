8e3eed239d4401e9d950417f902a45bf78703dd7
ksared <bartek876@gmail.com>
2022-08-03 00:29:37 +0200

    decision tree - halas viewmodel.

--- app/src/main/java/com/example/carfaultdetector/ViewModel/FaultDecisionTreeViewModel.java
+++ app/src/main/java/com/example/carfaultdetector/ViewModel/FaultDecisionTreeViewModel.java
@@ -1,6 +1,99 @@
 package com.example.carfaultdetector.ViewModel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class FaultDecisionTreeViewModel extends ViewModel {
+
+    public MutableLiveData<Integer> mutableLiveDataHalas = new MutableLiveData<>();
+    protected String wynik = "";
+
+    public String wynikHalas(boolean[] bool){
+        if(bool[0]){
+            if(bool[5]){
+                if(bool[10]){
+                    wynik = "uszkodzony silnik";
+                }
+                else if(bool[9]){
+                    wynik = "Czujnik położenia wału lub przepustnica";
+                }
+                else{
+                    wynik = "Usterka elektroniki silnika";
+                }
+            }
+            else{
+                wynik = "Prawdopodobne przyczyny: poduszka silnika, wtryski, turbosprężarka";
+            }
+
+        }
+        else if(bool[1]){
+            if(bool[6]){
+                wynik = "Nieprawidłowa zbieżność";
+            }
+            else if(bool[4]){
+                wynik = "nieprawidłowa geometria";
+            }
+            else if(bool[8]){
+                wynik = "Uszkodzony przegub";
+            }
+            else{
+                wynik = "uszkodzone zawieszenie";
+            }
+        }
+        else if(bool[2]){
+            if(bool[10]){
+                wynik = "Zapchany dpf lub filtr powietrza";
+            }
+            else{
+                wynik = "dziurawy układ wydechowy";
+            }
+        }
+        else if(bool[3]){
+            if(bool[4]){
+                wynik = "Uszkodzone klocki lub tarcze hamulcowe";
+            }
+            else{
+                wynik = "Uszkodzona piasta koła";
+            }
+        }
+        else if(bool[4]){
+            wynik = "Nieprawidłowa geometria";
+        }
+        else if(bool[5]){
+            wynik = "Zbyt mało informacji do stwierdzenia usterki";
+        }
+        else if(bool[6]){
+            if(bool[8]){
+                wynik = "Uszkodzone zawieszenie";
+            }
+            else{
+                wynik = "Zbyt mało informacji do stwierdzenia usterki";
+            }
+        }
+        else if(bool[7]){
+            if(bool[9]){
+                wynik = "Uszkodzona przepustnica";
+            }
+            else if(bool[10]){
+                wynik = "Uszkodzony czujnik obrotów wału";
+            }
+            else{
+                wynik = "Zbyt mało informacji do stwierdzenia usterki";
+            }
+        }
+        else if(bool[8]){
+            wynik = "Uszkodzone klocki lub tarcze hamulcowe";
+        }
+        else if(bool[9]){
+            wynik = "Możliwe uszkodzenia: przepustnica, turbosprężarka, wtryskiwacze";
+        }
+        else{
+            wynik = "Zbyt mało informacji do stwierdzenia usterki";
+        }
+        return wynik;
+    }
+
+    public String wynikOdpalanie(){
+        return  wynik;
+    }
 }
--- app/src/main/java/com/example/carfaultdetector/view/FaultDecisionTree.java
+++ app/src/main/java/com/example/carfaultdetector/view/FaultDecisionTree.java
@@ -2,21 +2,31 @@ package com.example.carfaultdetector.view;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.example.carfaultdetector.R;
+import com.example.carfaultdetector.ViewModel.FaultDecisionTreeViewModel;
 
 public class FaultDecisionTree extends AppCompatActivity {
 
     private AlertDialog.Builder alertDialogBuilder;
     private AlertDialog alertDialog;
+    private Intent intent;
 
     Button halasButton;
     Button odpalanieButton;
     Button inneButton;
     Button kontrolkiButton;
+
+    FaultDecisionTreeViewModel faultDecisionTreeViewModel;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,48 @@ public class FaultDecisionTree extends AppCompatActivity {
         odpalanieButton = findViewById(R.id.odpalanieTreeButton);
         inneButton = findViewById(R.id.othersTreeButton);
         kontrolkiButton = findViewById(R.id.controlTreeButton);
+        faultDecisionTreeViewModel = new ViewModelProvider(this).get(FaultDecisionTreeViewModel.class);
+
+
+        halasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.tree_halas);
+                CheckBox halasSilnika = findViewById(R.id.halastreeHalasSilnika);
+                CheckBox halasKola = findViewById(R.id.halastreeHalasKola);
+                CheckBox halasWydechu = findViewById(R.id.halastreeHalasWydechu);
+                CheckBox halasHamowanie = findViewById(R.id.halastreeHalasPodczasHamowania);
+                CheckBox bicieKierownicy = findViewById(R.id.halastreeBicieKierownicy);
+                CheckBox checkEngine = findViewById(R.id.controlTreeCheckEngine);
+                CheckBox sciaganie = findViewById(R.id.halastreeSciaganiePodczasJazdyWyszukiwanie);
+                CheckBox wibracje = findViewById(R.id.halastreeWibracjeSilnikaWyszukiwanie);
+                CheckBox halasSkrecanie = findViewById(R.id.halastreeHalasPodczasSkrecania);
+                CheckBox halasObroty = findViewById(R.id.halastreeHalasNaObrotach);
+                CheckBox szarpanie = findViewById(R.id.halastreeSzarpanie);
+
+
+            }
+        });
+
+        odpalanieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.tree_odpalanie);
+            }
+        });
+
+        inneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.tree_inne);
+            }
+        });
+
+        kontrolkiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.tree_kontrolki);
+            }
+        });
     }
 }
